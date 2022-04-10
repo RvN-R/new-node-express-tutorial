217276e7a8e82f82e381c8ceb746444dfa99632c
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-10 09:25:59 +0100

    Basic headers in place

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,9 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write("<h1>Home Page</h1>")
+    res.end()
+})
+
+server.listen(5000)
