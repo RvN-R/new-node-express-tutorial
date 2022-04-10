71ac357a3917703282bd5b43b2dd79ce51bc1e2a
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-10 09:35:21 +0100

    Basic Server Setup

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,26 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-    res.writeHead(200, {'content-type': 'text/html'})
-    res.write("<h1>Home Page</h1>")
-    res.end()
+    // console.log(req.method)
+    const url = req.url
+    // home page
+    if(url === '/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write("<h1>Home Page</h1>")
+        res.end() 
+    }
+    // about page
+    else if(url ==='/about'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write("<h1>About Page</h1>")
+        res.end()
+    }
+    //404
+    else{
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.write("<h1>Doesn't Exist</h1>")
+        res.end()
+    }
 })
 
 server.listen(5000)
