05bbeeddd1aef59e57b26ac8835859aed9f5b304
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-11 15:51:54 +0100

    Completed Route Paramters Exercise

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const app = express();
 
-const {products} = require('./data')
+const { products } = require('./data')
 
 app.get('/', (req,res) => {
-    res.json(products)
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req,res) =>{
+    const newProduct = products.map((product) => {
+        const {id,name,image} = product;
+        return {id, name, image}
+    })
+    res.json(newProduct)
+})
+
+app.get('/api/products/:productID', (req,res) =>{
+    const {productID} = req.params;
+
+    const singleProduct = products.find(
+        (product) => product.id === Number(productID)
+        )
+    if(!singleProduct){
+        return res.status(404).send('Product Does Not Exist')
+    } 
+    console.log(singleProduct)
+    res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req,res) => {
+    console.log(req.params);
+    res.send("Hello World")
 })
 
 app.listen(5000, () =>{
