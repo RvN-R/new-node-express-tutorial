8b951dd07cf596bc843c44ca94bafcfb7e202c68
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-11 16:11:28 +0100

    Query String Exercise Complete

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -33,6 +33,27 @@ app.get('/api/products/:productID/reviews/:reviewID', (req,res) => {
     res.send("Hello World")
 })
 
+app.get('/api/v1/query', (req,res) => {
+    // console.log(req.query)
+    const{ search,limit } = req.query
+    let sortedProducts = [...products];
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if(sortedProducts.length < 1){
+        // res.status(200).send('No Products Matched Your Search')
+        return res.status(200).json({success:true, data:[]})
+    }
+    res.status(200).json(sortedProducts)
+    
+})
+
 app.listen(5000, () =>{
     console.log("Server is listening on port 5000...")
 })
