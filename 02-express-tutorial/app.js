92952a1c0789c66bdd8fee7d5f710c3dd01af633
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-11 12:43:51 +0100

    Created public folder, used express static to render

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log('user hit the resource')
-    res.send("Welcome Home Page")
+// setup static and middlewares
+app.use(express.static('./public'))
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
-app.get('/about', (req,res) =>{
-    res.send("About Page")
+app.all('*', (req, res) => {
+    res.status(404).send('resource not found')
 })
 
-app.all('*', (req,res) => {
-    res.status(400).send("<h1>resource not found</h1>")
+app.listen(5000, () => {
+    console.log(' server is listening to port 5000....');
 })
-
-app.listen(5000, ()=>{
-    console.log("server is listening on port 5000....")
-})
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
