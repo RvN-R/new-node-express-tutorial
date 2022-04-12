8678675a1091f6351361f3833f14fb1b9494efc2
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-12 16:04:37 +0100

    Completed PUT Method exercises

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -42,6 +42,25 @@ app.post('/login', (req,res) => {
     res.status(404).send('Please Provide Credentails')
 })
 
+app.put('/api/people/:id', (req,res) => {
+    const {id} = req.params
+    const {name} = req.body
+    
+    const person = people.find((person) => person.id === Number(id))
+    if (!person) {
+        return res
+        .status(400)
+        .json({ success: false, msg: `no person with the id ${id}` })
+    }
+    const newPeople = people.map((person) => {
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success: true, data:newPeople})
+})
+
 app.listen(5000, () =>{
     console.log("Server is listening on port 5000...")
 })
