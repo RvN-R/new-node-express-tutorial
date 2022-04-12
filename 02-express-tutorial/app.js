0bcb8392372d952256cd707e1aeb2eea82ba1e5d
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-12 15:49:37 +0100

    Experimenting with Postman

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -23,6 +23,15 @@ app.post('/api/people', (req, res) => {
     res.status(201).json({ success: true, person: name })
 })
 
+app.post('/api/postman/people', (req, res) => {
+    const {name} = req.body
+    if (!name) {
+        return res
+        .status(400)
+        .json({ success: false, msg: 'please provide name value' })
+    }
+    res.status(201).json({ success: true, data: [...people, name] })
+})
 
 app.post('/login', (req,res) => {
     const {name} = req.body
