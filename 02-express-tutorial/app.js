9d28ec6d8ae24b2efbe7a5d0754b13ce9b6d1791
Richard Van Nairn <rvannairn@googlemail.com>
2022-04-12 16:15:21 +0100

    Completed Method DELETE Javascript Example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -61,6 +61,17 @@ app.put('/api/people/:id', (req,res) => {
     res.status(200).json({success: true, data:newPeople})
 })
 
+app.delete('/api/people/:id', (req,res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if (!person) {
+        return res
+            .status(404)
+            .json({ success: false, msg: `no person with the id ${req.params.id}` })
+    }
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id));
+    return res.status(200).json({success:true, data:newPeople })
+})
+
 app.listen(5000, () =>{
     console.log("Server is listening on port 5000...")
 })
